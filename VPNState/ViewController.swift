f5bab2471908469a43f515422778e21bb2f5f946
Hadi Sharghi <hsharghi@gmail.com>
2022-11-12 13:48:51 +0330

    Initial Commit

---
+++ VPNState/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  VPNState
+//
+//  Created by Hadi Sharghi on 11/12/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
